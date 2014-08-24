51563c92494bbd90c3211f7c99ce7bcab6c46320
Lucas Ponce <lponce@redhat.com>
2014-08-24 16:04:54 +0200

    JBoss Cache deadlock example

---
+++ src/main/java/TestConcurrentPut.java
@@ -0,0 +1,113 @@
+
+import org.jboss.cache.Cache;
+import org.jboss.cache.DefaultCacheFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
+
+/*
+ * Copyright (C) 2014 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+/**
+ * @author <a href="mailto:nfilotto@exoplatform.com">Nicolas Filotto</a>
+ * @version $Id$
+ *
+ */
+public class TestConcurrentPut
+{
+    // The default total amount of threads knowing that in udp-mux.xml,
+    // thread_pool.max_threads and oob_thread_pool.max_threads have been set to 20
+    private static final int DEFAULT_TOTAL_THREADS = 20;
+    // Volatile time variables shared between all threads to get measures of each iteration, no matter in which thread
+    private static volatile long time, newTime;
+
+    public static void main(final String... args) throws Exception
+    {
+        System.setProperty("jgroups.bind_addr", "127.0.0.1");
+
+        DefaultCacheFactory cacheFactory = new DefaultCacheFactory();
+        final Cache<String, String> cache = cacheFactory.createCache("test-jbosscache-config.xml", true);
+
+        System.out.println("Wait until the cluster is ready");
+        while (cache.getMembers() == null);
+        while (cache.getMembers().size() < 2);
+
+        System.out.println("The cluster is ready");
+        final CountDownLatch startSignal = new CountDownLatch(1);
+        final AtomicLong counter = new AtomicLong();
+        final String nodeId;
+        int totalThreads = DEFAULT_TOTAL_THREADS;
+        if (args.length > 0)
+        {
+            nodeId = args[0];
+            if (args.length > 1)
+            {
+                totalThreads = Integer.parseInt(args[1]);
+            }
+        }
+        else
+        {
+            nodeId = Integer.toString(System.identityHashCode(cache));
+        }
+        System.out.println("The id of this node is " + nodeId);
+        System.out.println("The total amount of threads for this node is " + totalThreads);
+        Runnable task = new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    startSignal.await();
+                }
+                catch (InterruptedException e1)
+                {
+                    e1.printStackTrace();
+                    return;
+                }
+                time = System.currentTimeMillis();
+                while (true)
+                {
+                    try
+                    {
+                        String value = cache.get("/region", "a");
+
+                        long call = counter.incrementAndGet();
+
+                        if ((call % 1000) == 0) {
+                            newTime = System.currentTimeMillis();
+                            long iterationTime= newTime - time;
+                            System.out.println(call + ". " + Thread.currentThread() + ": a=" + value + " from " + nodeId + " Iteration: " + iterationTime + " ms");
+                            time = newTime;
+                        }
+                        cache.put("/region", "a", nodeId);
+                        Thread.sleep(20);
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        for (int i = 0; i < totalThreads; i++)
+            new Thread(task).start();
+        startSignal.countDown();
+    }
+
+}
